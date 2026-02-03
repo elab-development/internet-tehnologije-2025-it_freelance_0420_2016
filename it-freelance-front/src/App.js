88d27888cbbcde0b438d4356deb3bcb3309f3c09
Dalibor Maksimovic <dmaksimovic97@gmail.com>
2026-02-03 15:12:30 +0000

    Sve uvezano u okviru korene komponente App.js i dodat dizajn

--- it-freelance-front/src/App.js
+++ it-freelance-front/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-function App() {
+import AppLayout from "./components/AppLayout";
+import Placeholder from "./components/Placeholder";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+import FreelancerHome from "./pages/FreelancerHome";
+import ClientHome from "./pages/ClientHome";
+import AdminHome from "./pages/AdminHome";
+
+import ClientProjects from "./pages/ClientProjects";
+import MyClientProjects from "./pages/MyClientProjects";
+
+import FreelancerProjects from "./pages/FreelancerProjects";
+
+import AdminCategories from "./pages/AdminCategories";
+import AdminMetrics from "./pages/AdminMetrics";
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {/* Public stranice bez menija */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* Sve ostalo ide kroz layout sa Menu */}
+      <Route element={<AppLayout />}>
+        <Route path="/freelancer" element={<FreelancerHome />} />
+        <Route path="/client" element={<ClientHome />} />
+        <Route path="/admin" element={<AdminHome />} />
+
+        {/* Rute koje su u meniju */}
+        <Route path="/admin/projects" element={<Placeholder title="Admin Projects" />} />
+
+        <Route path="/projects" element={<ClientProjects />} />
+        <Route path="/my-projects" element={<MyClientProjects />} />
+
+        <Route path="/freelancer/projects" element={<FreelancerProjects />} />
+
+        <Route path="/admin/categories" element={<AdminCategories />} />
+        <Route path="/admin/metrics" element={<AdminMetrics />} />
+
+      </Route>
+
+      {/* Fallback */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
 }
-
-export default App;
